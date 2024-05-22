1f66eeb893f0818a42d8b3b64bdf33f455596395
Khuu Viet Sang <sangkhuudev@gmail.com>
2024-05-22 11:18:08 +0700

    Setup connection between Rocket and Postgres

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use rocket_db_pools::Database;
+
 mod models;
 mod schema;
 mod repositories;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Database)]
+#[database("postgres")]
+struct DbConn(rocket_db_pools::diesel::PgPool);
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", rocket::routes![
+
+        ])
+        .attach(DbConn::init())
+        .launch()
+        .await;
 }
