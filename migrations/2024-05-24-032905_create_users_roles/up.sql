78c61eaa01ed4782961b27ae56340b6d320b1992
Khuu Viet Sang <sangkhuudev@gmail.com>
2024-05-24 11:19:51 +0700

    Create users,roles model

---
+++ migrations/2024-05-24-032905_create_users_roles/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users_roles (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    role_id INTEGER NOT NULL REFERENCES roles(id)
+)
--- src/models.rs
+++ src/models.rs
@@ -1,46 +1,95 @@
 use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
-use crate::schema::{rustaceans, crates};
+use crate::schema::{rustaceans, crates, users_roles, users, roles}; 
 
-#[derive(Debug, Clone, Queryable, AsChangeset, Serialize, Deserialize )]
+#[derive(Debug, Clone, Queryable, AsChangeset, Serialize, Deserialize)]
 pub struct Rustacean {
     #[serde(skip_deserializing)]
-    pub id : i32,
+    pub id: i32,
     pub name: String,
     pub email: String,
     #[serde(skip_deserializing)]
-    pub created_at: NaiveDateTime
+    pub created_at: NaiveDateTime,
 }
 
-
-#[derive(Debug, Clone, Insertable, Deserialize )]
-#[diesel(table_name=rustaceans)]
+#[derive(Debug, Clone, Insertable, Deserialize)]
+#[diesel(table_name = rustaceans)]
 pub struct NewRustacean {
     pub name: String,
     pub email: String,
 }
 
-#[derive(Debug, Clone, Queryable, AsChangeset, Serialize, Deserialize )]
+#[derive(Debug, Clone, Queryable, AsChangeset, Serialize, Deserialize)]
 pub struct Crate {
     #[serde(skip_deserializing)]
-    pub id : i32,
+    pub id: i32,
     pub rustacean_id: i32,
     pub code: String,
     pub name: String,
     pub version: String,
     pub description: Option<String>,
     #[serde(skip_deserializing)]
-    pub created_at: NaiveDateTime
+    pub created_at: NaiveDateTime,
 }
 
-
-#[derive(Debug, Clone, Insertable, Deserialize )]
-#[diesel(table_name=crates)]
+#[derive(Debug, Clone, Insertable, Deserialize)]
+#[diesel(table_name = crates)]
 pub struct NewCrate {
     pub rustacean_id: i32,
     pub code: String,
     pub name: String,
     pub version: String,
     pub description: Option<String>,
+}
+
+#[derive(Debug, Clone, Queryable, AsChangeset, Serialize, Deserialize)]
+pub struct User {
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+    #[serde(skip_deserializing)]
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Debug, Clone, Insertable, Deserialize)]
+#[diesel(table_name = users)]
+pub struct NewUser {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Debug, Clone, Queryable, AsChangeset, Serialize, Deserialize)]
+pub struct Role {
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub code: String,
+    pub name: String,
+    #[serde(skip_deserializing)]
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Debug, Clone, Insertable, Deserialize)]
+#[diesel(table_name = roles)]
+pub struct NewRole {
+    pub code: String,
+    pub name: String,
+}
+
+#[derive(Debug, Clone, Queryable, Serialize, Deserialize)]
+#[diesel(belongs_to(User))]
+#[diesel(belongs_to(Role))]
+pub struct UserRole {
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub user_id: i32,
+    pub role_id: i32,
+}
+
+#[derive(Debug, Clone, Insertable, Deserialize)]
+#[diesel(table_name = users_roles)]  
+pub struct NewUserRole {
+    pub user_id: i32,
+    pub role_id: i32,
 }
